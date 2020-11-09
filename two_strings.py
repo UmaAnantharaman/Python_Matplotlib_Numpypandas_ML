8b4a85e22761dab5fa3849c114292d431f31f6e9
UmaAnantharaman <44247436+UmaAnantharaman@users.noreply.github.com>
2020-11-09 20:43:11 +0530

    Two_strings

---
+++ two_strings.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+def twoStrings(s1, s2):
+    m1 = set(s1)
+    m2 = set(s2)
+    if set.intersection(m1,m2):
+        return "YES"
+    return "NO"
+ 
+if __name__ == '__main__':
+    t = int(input())
+    for _ in range(t):
+        first = str(input())
+        second = str(input())
+        print(twoStrings(first, second))
